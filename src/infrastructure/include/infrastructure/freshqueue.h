b2511f5b47d7f6d78c64a036ded1c95073ad0ed7
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2023-03-14 00:06:59 +0800

    refactor: add FreshQueueWithMutex

--- src/infrastructure/include/infrastructure/freshqueue.h
+++ src/infrastructure/include/infrastructure/freshqueue.h
@@ -1,12 +1,37 @@
 #pragma once
+#include <exception>
+#include <mutex>
 #include <queue>
+#include <vector>
 
-template <typename T> class FreshQueue {
+template <typename T> class FreshQueueWithMutex {
 public:
-  FreshQueue() {}
-  virtual ~FreshQueue() {}
-  std::size_t size() const { return m_queue.size(); }
+  FreshQueueWithMutex() = default;
+  FreshQueueWithMutex(const FreshQueueWithMutex &) = delete;
+  FreshQueueWithMutex(FreshQueueWithMutex &&) noexcept = delete;
+  FreshQueueWithMutex &operator=(const FreshQueueWithMutex &) = delete;
+  FreshQueueWithMutex &operator=(FreshQueueWithMutex &&) noexcept = delete;
+  virtual ~FreshQueueWithMutex() = default;
+
+  std::size_t size() const {
+    const std::lock_guard<std::mutex> lock{m_mutex};
+    return m_queue.size();
+  }
+  void push(const T &val) {
+    const std::lock_guard<std::mutex> lock{m_mutex};
+    m_queue.push(val);
+  }
+  T pop() {
+    const std::lock_guard<std::mutex> lock{m_mutex};
+    if (m_queue.empty()) {
+      throw std::runtime_error("pop from empty FreshQueueWithMutex!");
+    }
+    T val{m_queue.front()};
+    m_queue.pop();
+    return val;
+  }
 
 private:
   std::queue<T> m_queue;
+  mutable std::mutex m_mutex;
 };
--- test/infrastructure/infrastructure_test.cpp
+++ test/infrastructure/infrastructure_test.cpp
@@ -1,8 +1,18 @@
 #include "infrastructure/infrastructure.h"
 #include "gtest/gtest.h"
 
-TEST(FreshQueue, isInitiallyEmpty)
-{
-    FreshQueue<int> freshQueue{};
-    ASSERT_EQ(freshQueue.size(), 0);
+TEST(FreshQueueOfInts, initiallyEmptySize) {
+  FreshQueueWithMutex<int> freshQueue{};
+  ASSERT_EQ(freshQueue.size(), 0);
+}
+
+TEST(FreshQueueOfInts, initiallyEmptyPush) {
+  FreshQueueWithMutex<int> freshQueue{};
+  freshQueue.push(1);
+  ASSERT_EQ(freshQueue.size(), 1);
+}
+
+TEST(FreshQueueOfInts, initiallyEmptyPop) {
+  FreshQueueWithMutex<int> freshQueue{};
+  ASSERT_THROW(freshQueue.pop(), std::exception);
 }
