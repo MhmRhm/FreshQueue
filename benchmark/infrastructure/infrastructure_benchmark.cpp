fbacb53aedf5d0ed66010820ebecf46b09f18b77
Mohammad Rahimi <rahimi.mhmmd@gmail.com>
2023-03-15 07:54:28 +0800

    chore: add benchmark for FreshQueueWithMutex

--- benchmark/infrastructure/infrastructure_benchmark.cpp
+++ benchmark/infrastructure/infrastructure_benchmark.cpp
@@ -1,2 +1,49 @@
 #include "benchmark/benchmark.h"
 #include "infrastructure/infrastructure.h"
+#include "random"
+#include "vector"
+
+template <typename T>
+void BM_FreshQueueWithMutex_PushAndPopSingleThread(benchmark::State &state) {
+  FreshQueueWithMutex<T> freashQueue{};
+  for (auto _ : state) {
+    freashQueue.push(T{});
+    freashQueue.pop();
+  }
+  state.SetItemsProcessed(static_cast<int64_t>(state.iterations()));
+}
+BENCHMARK(BM_FreshQueueWithMutex_PushAndPopSingleThread<int>);
+
+template <typename T>
+void BM_FreshQueueWithMutex_ManyPushsThenManyPopsSingleThread(
+    benchmark::State &state) {
+  FreshQueueWithMutex<T> freashQueue{};
+  for (auto _ : state) {
+    for (int i = state.range(0); i--;)
+      freashQueue.push(T{});
+    for (int i = state.range(0); i--;)
+      freashQueue.pop();
+  }
+  state.SetItemsProcessed(
+      static_cast<int64_t>(state.iterations() * state.range(0)));
+}
+BENCHMARK(BM_FreshQueueWithMutex_ManyPushsThenManyPopsSingleThread<int>)
+    ->RangeMultiplier(2)
+    ->Range(1 << 0, 1 << 10);
+
+template <typename T>
+void BM_FreshQueueWithMutex_ManyPushsAndPopsSingleThread(
+    benchmark::State &state) {
+  FreshQueueWithMutex<T> freashQueue{};
+  for (auto _ : state) {
+    for (int i = state.range(0); i--;) {
+      freashQueue.push(T{});
+      freashQueue.pop();
+    }
+  }
+  state.SetItemsProcessed(
+      static_cast<int64_t>(state.iterations() * state.range(0)));
+}
+BENCHMARK(BM_FreshQueueWithMutex_ManyPushsAndPopsSingleThread<int>)
+    ->RangeMultiplier(2)
+    ->Range(1 << 0, 1 << 10);
--- test/infrastructure/infrastructure_test.cpp
+++ test/infrastructure/infrastructure_test.cpp
@@ -1,18 +1,41 @@
+#include "algorithm"
 #include "infrastructure/infrastructure.h"
+#include "ranges"
 #include "gtest/gtest.h"
 
-TEST(FreshQueueOfInts, initiallyEmptySize) {
+TEST(FreshQueueWithMutexOfInts, initiallyEmptySize) {
   FreshQueueWithMutex<int> freshQueue{};
   ASSERT_EQ(freshQueue.size(), 0);
 }
 
-TEST(FreshQueueOfInts, initiallyEmptyPush) {
+TEST(FreshQueueWithMutexOfInts, onePushSize) {
   FreshQueueWithMutex<int> freshQueue{};
-  freshQueue.push(1);
+  freshQueue.push(0);
   ASSERT_EQ(freshQueue.size(), 1);
 }
 
-TEST(FreshQueueOfInts, initiallyEmptyPop) {
+TEST(FreshQueueWithMutexOfInts, manyPushSize) {
+  using namespace std::views;
+  FreshQueueWithMutex<int> freshQueue{};
+  for (auto &&i : iota(0, 10)) {
+    freshQueue.push(i);
+  }
+  ASSERT_EQ(freshQueue.size(), 10);
+}
+
+TEST(FreshQueueWithMutexOfInts, manyPushPopSize) {
+  using namespace std::views;
+  FreshQueueWithMutex<int> freshQueue{};
+  for (auto &&i : iota(0, 10)) {
+    freshQueue.push(i);
+  }
+  for (auto &&i : iota(0, 5)) {
+    freshQueue.pop();
+  }
+  ASSERT_EQ(freshQueue.size(), 5);
+}
+
+TEST(FreshQueueWithMutexOfInts, initiallyEmptyPop) {
   FreshQueueWithMutex<int> freshQueue{};
   ASSERT_THROW(freshQueue.pop(), std::exception);
 }
